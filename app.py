cb00de5a8418767ee5aa8f21d923d1712a513102
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-10-15 22:15:54 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+import gspread as sg
+from gspread import Worksheet
+
+st.set_page_config(page_title="Painel de Adm - Webstore", page_icon="🟢", layout="wide")
+
+# with open("style.css") as f:
+#     st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
+
+
+st.divider()
+
+# ----------------------------------------------------------------------------------------
+# Dados Saídas
+
+gc = sg.service_account("key.json")
+url = 'https://docs.google.com/spreadsheets/d/1FZblAsihwNUfUVNDvdRQQ3SHx1PeOficVXcPLsQep3s/edit?usp=sharing'
+
+
+#df principal
+@st.cache_data
+def load_data(dados):
+    sh = gc.open_by_url(url)   
+    ws = sh.get_worksheet(0)   
+    planilha = ws.get_all_values()   
+    df = pd.DataFrame(planilha[1:], columns=planilha[0])
+    return df
+
+df = load_data(url)
+
+df = df.drop(columns="IMAGEM")
+
+
+
+dfeditar = df
+
+
+
+
+st.dataframe(df,use_container_width=True,hide_index=True)
