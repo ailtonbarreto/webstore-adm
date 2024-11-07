8b641136069cf926352f753fe766f540adbb58b5
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-07 20:04:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ SELECT
     tembo.tb_venda."SKU_CLIENTE", 
     tembo.tb_cliente."CLIENTE", 
     tembo.tb_produto."DESCRICAO",
+    tembo.tb_produto."CATEGORIA",
 FROM tembo.tb_venda
 JOIN tembo.tb_cliente ON tembo.tb_venda."SKU_CLIENTE" = tembo.tb_cliente."SKU_CLIENTE"
 JOIN tembo.tb_produto ON tembo.tb_venda."PARENT" = tembo.tb_produto."PARENT";
@@ -152,7 +153,7 @@ with card5:
     st.metric("Cancelados", f"{qtd_pedido_cancelado} 🔴".replace(',', 'X').replace('.', ',').replace('X', '.'))
     
 df_filtrado = df_filtrado.drop(columns=["data", "Ano","Mês","PARENT","SKU_CLIENTE"])
-df_filtrado = df_filtrado[["PEDIDO","EMISSAO","CLIENTE","DESCRICAO","QTD","VR_UNIT","TOTAL","STATUS"]]
+df_filtrado = df_filtrado[["PEDIDO","EMISSAO","CLIENTE","DESCRICAO","QTD","VR_UNIT","TOTAL","STATUS","CATEGORIA"]]
 
 st.dataframe(df_filtrado,hide_index=True,use_container_width=True)
 
