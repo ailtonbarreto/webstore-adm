d7570b021714f7bafd951210a1df2d5090568d9d
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-07 19:53:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,6 @@ SELECT
     tembo.tb_venda."SKU_CLIENTE", 
     tembo.tb_cliente."CLIENTE", 
     tembo.tb_produto."DESCRICAO",
-    tembo.tb_produto."CATEGORIA"
 FROM tembo.tb_venda
 JOIN tembo.tb_cliente ON tembo.tb_venda."SKU_CLIENTE" = tembo.tb_cliente."SKU_CLIENTE"
 JOIN tembo.tb_produto ON tembo.tb_venda."PARENT" = tembo.tb_produto."PARENT";
