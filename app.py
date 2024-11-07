03d1647593ebfb6ef412e98255ce86bbde20a1c7
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-07 20:49:39 -0300

    Y

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ SELECT
     tembo.tb_venda."STATUS",
     tembo.tb_venda."SKU_CLIENTE", 
     tembo.tb_cliente."CLIENTE", 
-    tembo.tb_produto."DESCRICAO_PAI",
+    tembo.tb_produto."DESCRICAO_PARENT",
     tembo.tb_produto."CATEGORIA"
 FROM tembo.tb_venda
 JOIN tembo.tb_cliente ON tembo.tb_venda."SKU_CLIENTE" = tembo.tb_cliente."SKU_CLIENTE"
--- select_bd_webstore.sql
+++ select_bd_webstore.sql
@@ -2,13 +2,13 @@ SELECT
     tembo.tb_venda."PEDIDO", 
     tembo.tb_venda."EMISSAO", 
     tembo.tb_venda."PARENT", 
-	tembo.tb_venda."QTD",
-	tembo.tb_venda."VR_UNIT",
-	tembo.tb_venda."STATUS",
+    tembo.tb_venda."QTD",
+    tembo.tb_venda."VR_UNIT",
+    tembo.tb_venda."STATUS",
     tembo.tb_venda."SKU_CLIENTE", 
     tembo.tb_cliente."CLIENTE", 
-    tembo.tb_produto."DESCRICAO"
-FROM tembo.tb_venda JOIN tembo.tb_cliente ON tembo.tb_venda."SKU_CLIENTE" = tembo.tb_cliente."SKU_CLIENTE"
+    tembo.tb_produto."DESCRICAO_PAI",
+    tembo.tb_produto."CATEGORIA"
+FROM tembo.tb_venda
+JOIN tembo.tb_cliente ON tembo.tb_venda."SKU_CLIENTE" = tembo.tb_cliente."SKU_CLIENTE"
 JOIN tembo.tb_produto ON tembo.tb_venda."PARENT" = tembo.tb_produto."PARENT";
-
-
