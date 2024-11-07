786880618670179d1dbc4412c5034a7abcdb8f98
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-07 19:23:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,20 @@ card1, card2, card3, card4, card5, card6, card7 = st.columns([2,2,2,2,2,1,1])
 # DATABASE POSTGRES NA NUVEM
 
 
-consulta = f'SELECT * FROM tembo.tb_venda'
+consulta = f'SELECT 
+    tembo.tb_venda."PEDIDO", 
+    tembo.tb_venda."EMISSAO", 
+    tembo.tb_venda."PARENT", 
+	tembo.tb_venda."QTD",
+	tembo.tb_venda."VR_UNIT",
+	tembo.tb_venda."STATUS",
+    tembo.tb_venda."SKU_CLIENTE", 
+    tembo.tb_cliente."CLIENTE", 
+    tembo.tb_produto."DESCRICAO"
+FROM tembo.tb_venda
+JOIN tembo.tb_cliente ON tembo.tb_venda."SKU_CLIENTE" = tembo.tb_cliente."SKU_CLIENTE"
+JOIN tembo.tb_produto ON tembo.tb_venda."PARENT" = tembo.tb_produto."PARENT";
+'
 
 
 @st.cache_data
