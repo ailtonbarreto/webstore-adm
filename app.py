eebbdf88a298c32983aa827419a25bae64c3ad5f
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-09 14:16:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -27,14 +27,16 @@ SELECT
     p."DESCRICAO_PARENT",
     v."QTD",
     v."VR_UNIT",
-    v."STATUS"
+    v."STATUS",
+    c."CLIENTE"  -- Seleciona o nome do cliente
 FROM 
     tembo.tb_venda AS v
 LEFT JOIN (
     SELECT DISTINCT ON ("PARENT") "PARENT", "DESCRICAO_PARENT"
     FROM tembo.tb_produto
     ORDER BY "PARENT"
-) AS p ON v."PARENT" = p."PARENT";
+) AS p ON v."PARENT" = p."PARENT"
+LEFT JOIN tembo.tb_cliente AS c ON v."SKU_CLIENTE" = c."SKU_CLIENTE";
 """
 
 
