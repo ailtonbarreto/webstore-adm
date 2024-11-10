401b91e758e22c8304cae8a1d9d2afcf2b285efb
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-10 15:54:49 -0300

    A

--- app.py
+++ app.py
@@ -268,7 +268,7 @@ with tab2:
             st.dataframe(df_filtrado_ped, use_container_width=True, hide_index=True)
     with col1b:
         df_ped_filtered = df.query('PEDIDO == @filtro_ped')
-        # df_ped_filtered = df_ped_filtered[["EMISSAO","PEDIDO","CLIENTE","DESCRICAO_PARENT","QTD","VR_UNIT","TOTAL","STATUS"]]
+        df_ped_filtered = df_ped_filtered[["EMISSAO","PEDIDO","CLIENTE","DESCRICAO_PARENT","QTD","VR_UNIT","TOTAL","STATUS"]]
         df_ped_filtered["EMISSAO"] = df_ped_filtered["EMISSAO"].dt.strftime('%d/%m/%Y')
         df_ped_filtered["TOTAL"] = df_ped_filtered["QTD"] * df_ped_filtered["VR_UNIT"]
         st.dataframe(df_ped_filtered, use_container_width=True, hide_index=True)
