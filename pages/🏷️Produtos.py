cf0d4276820112706a066b1c1c6da3c5278e1eab
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-10 20:38:58 -0300

    a

--- pages/🏷️Produtos.py
+++ pages/🏷️Produtos.py
@@ -22,3 +22,34 @@ descricao = st.text_input("Descrição")
 categoria = st.text_input("Categoria")
 
 vr_unit = st.number_input("Valor Unit")
+
+# Função para inserir dados (exemplo de INSERT)
+def insert_data(parent, sku, descricao_parent, descricao, categoria, vr_unit):
+    try:
+        conn = psycopg2.connect(
+            host='gluttonously-bountiful-sloth.data-1.use1.tembo.io',
+            database='postgres',
+            user='postgres',
+            password='MeSaIkkB57YSOgLO',
+            port='5432'
+        )
+
+        # Consulta SQL de INSERT com valores dinâmicos
+        insert_query = f"""
+        INSERT INTO tembo.tb_venda ("PARENT", "SKU", "DESCRICAO", "CATEGORIA", "VR_UNIT", "DESCRICAO_PARENT")
+        VALUES ({parent}, '{sku}', '{descricao}', '{categoria}', '{vr_unit}', {descricao_parent});
+        """
+
+        cursor = conn.cursor()
+        cursor.execute(insert_query)
+        conn.commit()
+
+        st.write("Dados inseridos com sucesso!")
+    except Exception as e:
+        st.write(f"Erro ao inserir dados: {e}")
+    finally:
+        if conn:
+            conn.close()
+            
+if st.button("💾"):
+    insert_data()
