ca2317d2902a0b6bc47c199bd467d1bb29e46ad7
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-09 21:55:04 -0300

    a

--- app.py
+++ app.py
@@ -98,41 +98,42 @@ def delete_order():
     port = '5432'
 
     filtro_ped = st.text_input("Pedido", placeholder="Pesquisar pedido")
+    with tab2:
+        with col1b:
+            if st.button("Excluir Pedido"):
+                try:
+                    # Tentando fazer a conexão com o banco de dados
+                    conn = psycopg2.connect(
+                        host=host,
+                        database=database,
+                        user=user,
+                        password=password,
+                        port=port
+                    )
+                    
+                    # Verificando se a conexão foi estabelecida
+                    if conn is None:
+                        st.error("Falha ao conectar ao banco de dados. Conexão retornou None.")
+                        return  # Saia da função se a conexão não foi bem-sucedida
 
-    if st.button("Excluir Pedido"):
-        try:
-            # Tentando fazer a conexão com o banco de dados
-            conn = psycopg2.connect(
-                host=host,
-                database=database,
-                user=user,
-                password=password,
-                port=port
-            )
-            
-            # Verificando se a conexão foi estabelecida
-            if conn is None:
-                st.error("Falha ao conectar ao banco de dados. Conexão retornou None.")
-                return  # Saia da função se a conexão não foi bem-sucedida
+                    with conn.cursor() as cursor:
+                        # Usando a interpolação correta para incluir a variável filtro_ped na string SQL
+                        query = f"""
+                        DELETE FROM tembo.tb_venda
+                        WHERE "PEDIDO" = '{filtro_ped}';
+                        """
+                        cursor.execute(query)  # Executando a query
+                        conn.commit()  # Confirma a transação
 
-            with conn.cursor() as cursor:
-                # Usando a interpolação correta para incluir a variável filtro_ped na string SQL
-                query = f"""
-                DELETE FROM tembo.tb_venda
-                WHERE "PEDIDO" = '{filtro_ped}';
-                """
-                cursor.execute(query)  # Executando a query
-                conn.commit()  # Confirma a transação
+                        st.success(f"Pedido {filtro_ped} excluído com sucesso.")
 
-                st.success(f"Pedido {filtro_ped} excluído com sucesso.")
-
-        except psycopg2.OperationalError as e:
-            st.error(f"Erro ao conectar ao banco de dados: {e}")
-        except Exception as e:
-            st.error(f"Erro ao executar a query ou excluir o pedido: {e}")
-        finally:
-            if conn:
-                conn.close()
+                except psycopg2.OperationalError as e:
+                    st.error(f"Erro ao conectar ao banco de dados: {e}")
+                except Exception as e:
+                    st.error(f"Erro ao executar a query ou excluir o pedido: {e}")
+                finally:
+                    if conn:
+                        conn.close()
 
 
 # -------------------------------------------------------------------------------------------------------
