05b081b9e82a5d51649daed426731d9f34d14e6e
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-11 20:51:18 -0300

    A

--- pages/🏷️Produtos.py
+++ pages/🏷️Produtos.py
@@ -63,7 +63,7 @@ with tab1:
         
        
         if not df_produto.empty:
-            st.subheader(F"PRODUTO {df_produto}")
+            st.subheader(F"PRODUTO {df_produto.DESCRICAO}")
             st.dataframe(df_produto, use_container_width=True)
         else:
             st.write("Nenhum produto encontrado para a pesquisa.")
