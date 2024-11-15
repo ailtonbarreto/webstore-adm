d853afd411807b191fe56c8589d428776b911cd4
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-15 18:57:28 -0300

    a

--- pages/🏷️Produtos.py
+++ pages/🏷️Produtos.py
@@ -55,24 +55,24 @@ with tab1:
     with col1:
         # Entrada de texto para pesquisa
         produto_filtro = st.text_input("Pesquisar pelo SKU ou Nome do Produto", "")
+    with col2:
+        if produto_filtro:  # Verifica se há algo no campo de pesquisa
+            # Filtra pelo SKU ou pelo nome (case insensitive)
+            df_produto = df[
+                df['SKU'].astype(str).str.contains(produto_filtro, case=False) |
+                df['DESCRICAO'].str.contains(produto_filtro, case=False)
+            ]
 
-    if produto_filtro:  # Verifica se há algo no campo de pesquisa
-        # Filtra pelo SKU ou pelo nome (case insensitive)
-        df_produto = df[
-            df['SKU'].astype(str).str.contains(produto_filtro, case=False) |
-            df['DESCRICAO'].str.contains(produto_filtro, case=False)
-        ]
-
-        if not df_produto.empty:  # Se houver resultados
-            st.write(f"**Foram encontrados {len(df_produto)} produto(s):**")
-            # Loop para exibir cada produto encontrado
-            for index, row in df_produto.iterrows():
-                st.subheader(row['DESCRICAO'], anchor=False)
-                st.image(row['IMAGEM'], width=200)
-                st.text(f"SKU: {row['SKU']}")
-                st.markdown("---")  # Linha separadora entre produtos
-        else:
-            st.write("Nenhum produto encontrado.")
+            if not df_produto.empty:  # Se houver resultados
+                st.write(f"**Foram encontrados {len(df_produto)} produto(s):**")
+                # Loop para exibir cada produto encontrado
+                for index, row in df_produto.iterrows():
+                    st.subheader(row['DESCRICAO'], anchor=False)
+                    st.image(row['IMAGEM'], width=200)
+                    st.text(f"SKU: {row['SKU']}")
+                    st.markdown("---")  # Linha separadora entre produtos
+            else:
+                st.write("Nenhum produto encontrado.")
 
 
 
