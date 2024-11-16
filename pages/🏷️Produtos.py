719664540fc607c77b6d4fc1b75431ad1d9923c9
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-16 20:30:46 -0300

    Update 🏷️Produtos.py

--- pages/🏷️Produtos.py
+++ pages/🏷️Produtos.py
@@ -67,7 +67,7 @@ with tab1:
 
                 if not df_produto.empty:
                     with col2:
-                        st.write(f"{len(df_produto)} produto(s)**",anchor=False)
+                        st.write(f"{len(df_produto)} produto(s)",anchor=False)
                     for index, row in df_produto.iterrows():
                         st.subheader(row['DESCRICAO'], anchor=False)
                         st.image(row['IMAGEM'], width=200)
