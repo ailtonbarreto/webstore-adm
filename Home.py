eed0a434b3466bb174d09a23b36e4f57251b046e
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-16 01:14:55 -0300

    a

--- Home.py
+++ Home.py
@@ -183,7 +183,7 @@ df_categoria = df_categoria.sort_values(by="TOTAL",ascending=True)
 grafico_barras = px.bar(df_categoria, x="TOTAL",y="CATEGORIA",orientation="h",color_discrete_sequence=["#0B1548"],
                         text=df_categoria["TOTAL"].apply(lambda x: f'R$ {x:,.2f}'))
 grafico_barras.update_yaxes(showgrid=False)
-grafico_barras.update_traces(showlegend=False,textfont=dict(size=15,color='#D8D9DF'),textposition="auto")
+grafico_barras.update_traces(showlegend=False,textfont=dict(size=15,color='#384792'),textposition="auto")
 grafico_barras.update_yaxes(showgrid=False,visible=True,title="")
 grafico_barras.layout.xaxis.fixedrange = True
 grafico_barras.layout.yaxis.fixedrange = True
