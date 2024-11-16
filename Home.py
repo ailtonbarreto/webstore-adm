b19ad676d1729ded96284ed2b4e40af68bf97ebc
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-16 01:18:21 -0300

    a

--- Home.py
+++ Home.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="ERP MODELO", page_icon="📊", layout="wide",init
 with open("style.css") as f:
     st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
 
-st.image("header.png",width=300)
+st.sidebar.image("header.png",width=300)
 
 
     
