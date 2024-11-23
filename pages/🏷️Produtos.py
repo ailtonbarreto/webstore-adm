7279d688a4f3dae68527eda206d5c717c2a42c6f
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-23 15:26:36 -0300

    a

--- pages/🏷️Produtos.py
+++ pages/🏷️Produtos.py
@@ -65,7 +65,6 @@ with tab1:
 
                 if not df_produto.empty:
                     with col3:
-                       
                         for index, row in df_produto.iterrows():
                             st.image(row['IMAGEM'], width=200)
                             st.subheader(row['DESCRICAO'], anchor=False)
