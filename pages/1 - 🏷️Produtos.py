e85a2341cc092870eec4eb8d51f6727f602644ea
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-23 15:39:18 -0300

    a

--- pages/1 - 🏷️Produtos.py
+++ pages/1 - 🏷️Produtos.py
@@ -58,6 +58,7 @@ with tab1:
     with col1:
         produto_filtro = st.text_input("Pesquise SKU", placeholder="Digite e tecle Enter")
         produto_filtro = produto_filtro.upper()
+        st.write(produto_filtro)
         with col2:
             if produto_filtro:
                 
