623fd06219ebe7418e04031e5b2fce8f973ce0ed
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-23 16:10:14 -0300

    a

---
+++ pages/2 - Clientes.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import psycopg2
+import pandas as pd
+
+st.set_page_config(page_title="Painel de Adm - Webstore", page_icon="📊", layout="wide",initial_sidebar_state="collapsed")
+
+with open("style.css") as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+
+tab1, tab2 = st.tabs(["Pesquisar Produto", "Cadastrar Produto"])
+
+
+# -------------------------------------------------------------------------------------------------------
+
+@st.cache_data
+def load_clientes():
+    host = 'gluttonously-bountiful-sloth.data-1.use1.tembo.io'
+    database = 'postgres'
+    user = 'postgres'
+    password = 'MeSaIkkB57YSOgLO'
+    port = '5432'
+
+    try:
+        conn = psycopg2.connect(
+            host=host,
+            database=database,
+            user=user,
+            password=password,
+            port=port
+        )        
+      
+        query = "SELECT * FROM tembo.tb_cliente;"
+        
+        df = pd.read_sql_query(query, conn)
+    except Exception as e:
+        st.write(f"Erro ao conectar: {e}")
+    
+
+    if conn:
+        conn.close()
+    return df
+
+df = load_clientes()
+
+st.dataframe(df)
