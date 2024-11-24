5bf6aaf8ca09c43fed78ea6868c05fd78605d631
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-24 13:41:34 -0300

    a

--- pages/1 - 🏷️Produtos.py
+++ pages/1 - 🏷️Produtos.py
@@ -120,8 +120,8 @@ with tab2:
             vr_unit = df_parent["VR_UNIT"].values[0]
             url = df_parent["IMAGEM"].values[0]
             variacao = st.selectbox("Variação",["UN","P","M","G","GG","EG","34","35","36","37","38","39","40","41","42","43","44"])
-            sku = parent + variacao
-            st.write(sku)
+            # sku = parent + variacao
+            # st.write(sku)
 
 
 # ----------------------------------------------------------------------------------------------
