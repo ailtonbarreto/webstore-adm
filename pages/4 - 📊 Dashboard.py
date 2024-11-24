cbf12729efbed453f12901c5c3b03a1584d31748
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-24 19:43:54 -0300

    a

--- pages/4 - 📊 Dashboard.py
+++ pages/4 - 📊 Dashboard.py
@@ -12,7 +12,7 @@ with open("style.css") as f:
     st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
 
 
-card1, card2, card3, card4, card5, card6, card7, = st.columns([2,2,2,2,2,1,1])
+card1, card2, card3, card4, card6, card7, = st.columns([2,2,2,2,1,1])
 col1, col2 = st.columns(2)
 col3, col4= st.columns(2)
 st.divider()
@@ -157,8 +157,8 @@ with card3:
 with card4:
     st.metric("QTD Clientes",f'👔{qtd_clientes:,.0f}'.replace(',', 'X').replace('.', ',').replace('X', '.'))
 
-with card5:
-    st.metric("Tx Cancelamentos", f"🔴{tx_cancelamento:.2f}%")
+# with card5:
+#     st.metric("Tx Cancelamentos", f"🔴{tx_cancelamento:.2f}%")
         
 # --------------------------------------------------------------------------------------
 # graficos
