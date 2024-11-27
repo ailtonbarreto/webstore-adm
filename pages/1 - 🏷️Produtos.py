7888b302ee397fcab5bac93ca6e1d959486a51b1
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-26 22:33:24 -0300

    a

--- pages/1 - 🏷️Produtos.py
+++ pages/1 - 🏷️Produtos.py
@@ -246,7 +246,7 @@ with tab2:
         else:
                 if st.button("Cadastrar Variação 💾"):
                     if sku and descricao and categoria and vr_unit > 0:
-                        insert_variacao(parent, sku, descricao, categoria, vr_unit, ativo)
+                        insert_variacao(parent, sku, descricao, categoria, vr_unit)
                         st.success("Produto inserido com sucesso!")
                         sleep(1)
                         st.cache_data.clear()
