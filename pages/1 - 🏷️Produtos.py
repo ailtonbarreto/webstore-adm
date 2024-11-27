5ca13cd357bf2b7066eba98bba72187fcac94d4f
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-26 21:19:06 -0300

    a

--- pages/1 - 🏷️Produtos.py
+++ pages/1 - 🏷️Produtos.py
@@ -158,7 +158,7 @@ with tab2:
             else:
                 produto_pai = st.selectbox("Produto Pai", df_parent["DESCRICAO_PARENT"].unique())
                 df_parent = df.query('DESCRICAO_PARENT == @produto_pai')
-                parent = df_parent["PARENT"].values[0]
+                parent = df_parent["DESCRICAO_PARENT"].values[0]
                 categoria = df_parent["CATEGORIA"].values[0]
                 vr_unit = df_parent["VR_UNIT"].values[0]
                 url = df_parent["IMAGEM"].values[0]
