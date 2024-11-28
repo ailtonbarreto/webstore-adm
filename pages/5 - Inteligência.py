2017220a28567e2d4f4a5e40225eaa1b52a51816
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-28 14:58:50 -0300

    a

---
+++ pages/5 - Inteligência.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+
+
+
+
+st.set_page_config(page_title="Inteligência", page_icon="📊", layout="wide",initial_sidebar_state="collapsed")
+
+with open("style.css") as f:
+    st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
+    
+    
+st.title("Dashboard Faturamento",anchor=False)
+bi = "https://app.powerbi.com/view?r=eyJrIjoiMmJiNDhkYWQtNWEzZS00YjQxLTkxNzctYWZiOWZlYWFjZTUyIiwidCI6IjE4Njc3OGFjLWJjNjMtNGE0My1iMmVlLThiMTk0MWVjYTRhOSJ9"
+components.iframe(bi, height=800)
+    
