c4aeb7b48bb135a0a0e6ef8d731d969ab5ac9baf
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-28 15:31:05 -0300

    a

--- pages/5 - 🧠 Inteligência.py
+++ pages/5 - 🧠 Inteligência.py
@@ -2,9 +2,6 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 
-
-
-
 st.set_page_config(page_title="Inteligência", page_icon="📊", layout="wide",initial_sidebar_state="collapsed")
 
 with open("style.css") as f:
