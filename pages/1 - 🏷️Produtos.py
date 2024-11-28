bdeefa86fd30123cff0f80b0764044555e5476fd
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-27 23:31:11 -0300

    a

--- pages/1 - 🏷️Produtos.py
+++ pages/1 - 🏷️Produtos.py
@@ -138,7 +138,7 @@ with tab1:
                         st.divider()
            
                         st.subheader("SKU do Produto",anchor=False)
-                        st.write(f"SKU: {row['SKU']}",ancor=False)
+                        st.write(f"{row['SKU']}",ancor=False)
                         
                         st.divider()
         
