f871d898244e5e40c6a8a082c16883cbc331ce33
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-28 15:34:08 -0300

    a

--- pages/5 - 🧠 Inteligência.py
+++ pages/5 - 🧠 Inteligência.py
@@ -8,5 +8,5 @@ with open("style.css") as f:
     st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
     
 
-components.iframe('<iframe title="Webstore" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMmJiNDhkYWQtNWEzZS00YjQxLTkxNzctYWZiOWZlYWFjZTUyIiwidCI6IjE4Njc3OGFjLWJjNjMtNGE0My1iMmVlLThiMTk0MWVjYTRhOSJ9" frameborder="0" allowFullScreen="true"></iframe>')
+components.iframe('<iframe title="Webstore" width="600" height="1000" src="https://app.powerbi.com/view?r=eyJrIjoiMmJiNDhkYWQtNWEzZS00YjQxLTkxNzctYWZiOWZlYWFjZTUyIiwidCI6IjE4Njc3OGFjLWJjNjMtNGE0My1iMmVlLThiMTk0MWVjYTRhOSJ9" frameborder="0" allowFullScreen="true"></iframe>')
     
