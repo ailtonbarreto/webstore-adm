faf2542c8c4577e9fe6a320ca8521600f48fa1f5
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-28 15:39:26 -0300

    a

--- pages/5 - 🧠 Inteligência.py
+++ pages/5 - 🧠 Inteligência.py
@@ -4,7 +4,9 @@ import streamlit.components.v1 as components
 
 st.set_page_config(page_title="Inteligência", page_icon="📊", layout="wide",initial_sidebar_state="collapsed")
 
-
+with open("style.css") as f:
+    st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
+    
 
 components.iframe("dashboard-webstore.html",height=1000)
     
