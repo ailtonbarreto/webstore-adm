b9f5ab647023d307a290a40d31aa79f0da7bd50a
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-12-08 12:22:27 -0300

    A

--- pages/4 - 📊 Dashboard.py
+++ pages/4 - 📊 Dashboard.py
@@ -200,7 +200,7 @@ df_tb["TOTAL"] = df_tb["TOTAL"].apply(lambda x: f'R$ {x:,.2f}')
 
 df_dispersao = df_filtrado.groupby("CLIENTE")["TOTAL"].sum().reset_index()
 
-dispersao_chart = px.scatter(df_dispersao,x="TOTAL",y="CLIENTE",size="TOTAL")
+dispersao_chart = px.scatter(df_dispersao,x="TOTAL",y="CLIENTE",size="TOTAL",color="CLIENTE")
 dispersao_chart.update_yaxes(showgrid=False,visible=False,title="")
 dispersao_chart.layout.xaxis.fixedrange = True
 dispersao_chart.layout.yaxis.fixedrange = True
