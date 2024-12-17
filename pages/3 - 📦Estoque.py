46324e1c8e8303c6d87c0a8ede313a043b68194f
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-12-16 23:33:39 -0300

    a

--- pages/3 - 📦Estoque.py
+++ pages/3 - 📦Estoque.py
@@ -220,7 +220,7 @@ with tab2:
         
         data = datetime.today()
         
-        variacao = opcoes.split("-")[1]
+        variacao = opcoes.split("-")
 
 
         texto_btn = "Entrada" if tipo == "E" else "Saída"
