a53e40c169fafb51d9d3823bc0829f3109ab1603
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2025-03-22 21:22:49 -0300

    updated

--- pages/4 - 📊 Dashboard.py
+++ pages/4 - 📊 Dashboard.py
@@ -263,20 +263,6 @@ style1 = """
 st.markdown(style1, unsafe_allow_html=True)
 
 
-# style2 = """
-#     <style>
-#     [data-testid="stFullScreenFrame"]
-#     {
-#     display: flex;
-#     justify-content: center;
-#     }
-#     </style>
-# """
-# st.markdown(style2, unsafe_allow_html=True)
-
-
-
-
 style4 = """
     <style>
     [data-testid="stMetricValue"]
